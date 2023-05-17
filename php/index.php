59c903c6ef8bee8c49f19c5d5e71c1d46b390f4c
hung222 <90371118+hung222@users.noreply.github.com>
2023-05-17 09:21:57 +0700

    Update index.php

--- php/index.php
+++ php/index.php
@@ -3,7 +3,7 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-
+$url="bbcdoe"
 // Check if image file is a actual image or fake image
 if(isset($_POST["submit"])) {
   $check = imagestring($_FILES["fileToUpload"]["tmp_name"]);
